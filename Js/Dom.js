6396cd3fdbb8b21025e381b7e6aa1b7e007bc6db
Progga <proggaroy2000@gmail.com>
2023-02-13 01:15:35 +0600

    append child added

--- Js/Dom.js
+++ Js/Dom.js
@@ -41,14 +41,48 @@ for(const section of sections){
   section.style.marginBottom = '5px';
   section.style.padding='10px';
   section.style.backgroundColor='lightGray'
-  console.log(section);
+  // console.log(section);
 }
 // const fruitSection = document.getElementById('fruit-section');
 // fruitSection.style.backgroundColor ='lightBlue'
 
-const fruitSection = document.getElementsById('fruit-section');
-fruitSection.classList.add('background');
-fruitSection.classList.remove('large-text');
+// const fruitSection = document.getElementById('fruit-section');
+// fruitSection.classList.add('background');
+// fruitSection.classList.remove('large-text');
+
+const placeList = document.getElementById('places-list');
+const li = document.createElement('li')
+li.innerText = 'Rangamati'
+placeList.appendChild(li);
 
 
+// const mainContainer = document.getElementById('main-container');
+// const section = document.createElement('section');
+// h1.innerText= 'My Dream'
+// section.appendChild(h1);
+// const ul = document.createElement('ul')
+// const li = document.createElement('li')
+// li.innerText = 'Become a real human'
+// h1.appendChild(ul);
+// mainContainer.appendChild(section)
+// vvvvvvvvvvvv
+// vvvvvvvvvvvv
+// const h1 = document.createElemeynt('h1');
 
+const mainContainer =  document.getElementById('main-container');
+const section = document.createElement('section');
+const h1 = document.createElement('h1');
+h1.innerText = 'My Dream';
+section.appendChild(h1);
+const ul = document.createElement('ul')
+const li1 = document.createElement('li')
+li1.innerText = 'Want to became a real human'
+ul.appendChild(li1)
+const li2 = document.createElement('li')
+li2.innerText = 'Want to became a web developer'
+ul.appendChild(li2)
+const li3 = document.createElement('li')
+li3.innerText = 'Want to became a real human'
+ul.appendChild(li3)
+section.appendChild(ul)
+mainContainer.appendChild(section)
