4ecca1e0b586b471e04833bf75a4f19fbf7081ea
Progga <proggaroy2000@gmail.com>
2023-02-12 20:40:05 +0600

    css in js

--- Js/Dom.js
+++ Js/Dom.js
@@ -2,13 +2,13 @@
 //  get element by Tag Name
 const liCollection = document.getElementsByTagName('li')
 for (const li of liCollection) {
-  console.log(li.innerText);
+  // console.log(li.innerText);
 }
 // /  get element by Tag Name
 const allHeadings = document.getElementsByTagName('h1')
 for (const h1 of allHeadings) {
-  console.log(h1.innerText);
-  console.log(h1.innerHTML);
+  // console.log(h1.innerText);
+  // console.log(h1.innerHTML);
 }
 // get element by ID
 const fruits = document.getElementById('fruit-title');
@@ -17,16 +17,38 @@ fruits.innerText = 'Changed title by JS';
 // /  get element by Class Name
 const places = document.getElementsByClassName('fav-places');
 for (const place of places) {
-  console.log(place.innerText)
+  // console.log(place.innerText)
 }
 
 //  querySelectorAll(return all)
 const someLi = document.querySelectorAll('.dom-section li');
 for (const li of someLi) {
-  console.log(li);
+  // console.log(li);
 }
 
 //  querySelector(return first one)
 const firstFruit = document.querySelector('#fruit-section li');
-console.log(firstFruit);
+// console.log(firstFruit);
+
+const text = document.getElementsByClassName('place-section');
+const textAgain = text.innerText;
+// console.log(textAgain);
+
+const sections = document.querySelectorAll('section');
+for(const section of sections){
+  section.style.border= '2px solid black'
+  section.style.borderRadius= '8px'
+  section.style.marginBottom = '5px';
+  section.style.padding='10px';
+  section.style.backgroundColor='lightGray'
+  console.log(section);
+}
+// const fruitSection = document.getElementById('fruit-section');
+// fruitSection.style.backgroundColor ='lightBlue'
+
+const fruitSection = document.getElementsById('fruit-section');
+fruitSection.classList.add('background');
+fruitSection.classList.remove('large-text');
+
+
 
