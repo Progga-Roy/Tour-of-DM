cfed87b253be85c8437787ae6f1b1995f4b6b8f0
Progga <proggaroy2000@gmail.com>
2023-02-12 16:40:04 +0600

    querySelectorAll and querySelector

--- Js/Dom.js
+++ Js/Dom.js
@@ -1,20 +1,32 @@
-// console.log('Hello from JS')
 
+//  get element by Tag Name
 const liCollection = document.getElementsByTagName('li')
-for(const li of liCollection){
-    console.log(li.innerText);
+for (const li of liCollection) {
+  console.log(li.innerText);
 }
+// /  get element by Tag Name
 const allHeadings = document.getElementsByTagName('h1')
-    for(const h1 of allHeadings){
-      console.log(h1.innerText);
-      console.log(h1.innerHTML);
-    }
-
-    const fruits = document.getElementById('fruit-title');
-    fruits.innerText= 'Changed title by JS';
-
-
-const places = document.getElementsByClassName('fav-places');
-for(const place of places){
-console.log(place.innerText)
+for (const h1 of allHeadings) {
+  console.log(h1.innerText);
+  console.log(h1.innerHTML);
 }
+// get element by ID
+const fruits = document.getElementById('fruit-title');
+fruits.innerText = 'Changed title by JS';
+
+// /  get element by Class Name
+const places = document.getElementsByClassName('fav-places');
+for (const place of places) {
+  console.log(place.innerText)
+}
+
+//  querySelectorAll(return all)
+const someLi = document.querySelectorAll('.dom-section li');
+for (const li of someLi) {
+  console.log(li);
+}
+
+//  querySelector(return first one)
+const firstFruit = document.querySelector('#fruit-section li');
+console.log(firstFruit);
+
