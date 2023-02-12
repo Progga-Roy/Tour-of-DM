2c38c748c70b8d2371f93b47ad488972d2b8f3a3
Progga <proggaroy2000@gmail.com>
2023-02-12 13:08:31 +0600

    some js tag added

--- Js/Dom.js
+++ Js/Dom.js
@@ -1,1 +1,12 @@
-console.log('Hello from JS')
+// console.log('Hello from JS')
+
+const liCollection = document.getElementsByTagName('li')
+for(const li of liCollection){
+    console.log(li.innerText);
+}
+const allHeadings = document.getElementsByTagName('h1')
+    for(const h1 of allHeadings){
+      console.log(h1.innerText);
+      console.log(h1.innerHTML);
+    }
+
